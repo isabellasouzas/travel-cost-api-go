8a4b908da84f8eaa3dfdaa662f5973f86d54a3ed
isabsantos92@gmail.com <isabsantos92@gmail.com>
2020-08-28 01:12:45 -0300

    adiciona runner.sh

---
+++ scripts/run_database.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+docker start be9a7449361a
+docker cp /data/data.json be9a7449361a:/tmp/data.json
+docker exec be9a7449361a mongoimport -d quickstart -c reservations --file /tmp/data.json --jsonArray
+echo "Database is running..."
+
