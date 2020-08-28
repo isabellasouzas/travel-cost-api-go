5ab7ad9b9406da5ba36f5af20f9c179cb249fde5
isabsantos92@gmail.com <isabsantos92@gmail.com>
2020-08-27 21:36:54 -0300

    cria rotas e servidor que funcionam

--- main.go
+++ main.go
@@ -1,28 +1,46 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"github.com/bellasouzas/travel-cost-api-go/db_config"
-	"go.mongodb.org/mongo-driver/bson"
+	"io"
+	//"context"
+	//"fmt"
+	//"github.com/bellasouzas/travel-cost-api-go/db_config"
+	//"github.com/bellasouzas/travel-cost-api-go/routers"
+	//"go.mongodb.org/mongo-driver/bson"
+	"github.com/bellasouzas/travel-cost-api-go/routers"
 	"log"
+	"net/http"
 )
 
 func main() {
 
-	conn := db_config.GetMongoConnection()
-	collection := conn.Database("quickstart").Collection("reservations")
-
-	fmt.Println("Collection accessed!")
-	cursor, err := collection.Find(context.TODO(), bson.M{})
-	if err != nil {
-		log.Fatal(err)
-	}
-	var travelInfo []bson.M
-	if err = cursor.All(context.TODO(), &travelInfo); err != nil {
-		log.Fatal(err)
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Hello, world!\n")
 	}
 
-	fmt.Println(travelInfo)
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/getalldata", routers.GetAllData)
+	http.HandleFunc("/totalcostreservartion", routers.TotalCostReservations)
+	http.HandleFunc("/totalcostpermonth", routers.TotalCostPerMonth)
+	http.HandleFunc("/findbyname", routers.FindByName)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	//
+	//
+	//conn := db_config.GetMongoConnection()
+	//collection := conn.Database("quickstart").Collection("reservations")
+	//
+	//fmt.Println("Collection accessed!")
+	//cursor, err := collection.Find(context.TODO(), bson.M{})
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//var travelInfo []bson.M
+	//if err = cursor.All(context.TODO(), &travelInfo); err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//fmt.Println(travelInfo)
 
 }
--- routers/main.go
+++ routers/main.go
@@ -1,1 +1,26 @@
 package routers
+
+import (
+	"io"
+	"net/http"
+)
+
+//GetAllData
+func GetAllData(res http.ResponseWriter, req *http.Request) {
+	io.WriteString(res, "GetAllData!\n")
+}
+
+//TotalCostReservations
+func TotalCostReservations(res http.ResponseWriter, req *http.Request) {
+	io.WriteString(res, "TotalCostReservations!\n")
+}
+
+//TotalCostPerMonth
+func TotalCostPerMonth(res http.ResponseWriter, req *http.Request) {
+	io.WriteString(res, "TotalCostPerMonth!\n")
+}
+
+//FindByName
+func FindByName(res http.ResponseWriter, req *http.Request) {
+	io.WriteString(res, "FindbyName!\n")
+}
