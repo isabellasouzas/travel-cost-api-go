78d64ba66b84e70333a97e8824b374e6a3ad174a
isabsantos92@gmail.com <isabsantos92@gmail.com>
2020-09-02 14:15:48 -0300

    cria SeedReservations func e Truncate func

---
+++ testdb/testdb.go
@@ -0,0 +1,53 @@
+package testdb
+
+import (
+	"context"
+	"github.com/bellasouzas/travel-cost-api-go/models"
+	"github.com/bellasouzas/travel-mongodb-go/helper"
+	"log"
+	"os"
+	"testing"
+	"time"
+)
+
+// SeedList creates new values to database to be tested
+func SeedReservations() {
+	//"DatadeInicio" and "DataFinal"
+	datadeInicio1, _ := time.Parse(time.RFC3339, "1969-12-8T21:00:00-01:00")
+	datadeFim1, _ := time.Parse(time.RFC3339, "1969-12-10T21:00:00-05:00")
+	datadeInicio2, _ := time.Parse(time.RFC3339, "1969-12-20T21:00:00-02:00")
+	datadeFim2, _ := time.Parse(time.RFC3339, "1969-12-23T21:00:00-03:00")
+
+	var reservations = map[string]models.DataTravel{
+		"001": {IdentificadorDoProcesso: 15045825, Situacao: "Realizada", ViagemUrgente: "NAO",
+			NomeDoOrgaoSuperior: "Ministerio da Educaçao", Nome: "MARINA FERREIRA KITAZONO ANTUNES", Cargo: "",
+			DataDeInicio: datadeInicio1, DataDeFim: datadeFim1, Destinos: "Recife/PE", ValorDiarias: 0,
+			ValorPassagens: 3406.33, ValorOutrosGastos: 0,
+		},
+		"002": {
+			IdentificadorDoProcesso: 15100682, Situacao: "Realizada", ViagemUrgente: "NAO",
+			NomeDoOrgaoSuperior: "Ministerio da Educaçao", Nome: "JORGE ANDRE DE CARVALHO MENDONCA", Cargo: "",
+			DataDeInicio: datadeInicio2, DataDeFim: datadeFim2, Destinos: "Recife/PE",
+			ValorDiarias: 0, ValorPassagens: 2925.83,
+			ValorOutrosGastos: 0},
+	}
+
+	collection := helper.ConnectDB()
+	ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
+	for _, datatravel := range reservations {
+		if err, _ := collection.InsertOne(ctx, datatravel); err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func Truncate() {
+	collection := helper.ConnectDB()
+	ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
+	collection.Drop(ctx)
+	SeedReservations()
+}
+
+func TestMain(m *testing.M) {
+	os.Exit(TestMain(m))
+}
