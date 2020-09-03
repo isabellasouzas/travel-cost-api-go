c96246ad03e75b41908eda6f638e9108f703a0bf
isabsantos92@gmail.com <isabsantos92@gmail.com>
2020-09-03 01:17:47 -0300

    implementa TestMain func

--- testdb/testdb.go
+++ testdb/testdb.go
@@ -4,15 +4,17 @@ import (
 	"context"
 	"github.com/bellasouzas/travel-cost-api-go/models"
 	"github.com/bellasouzas/travel-mongodb-go/helper"
+	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 	"testing"
 	"time"
 )
 
 // SeedList creates new values to database to be tested
 func SeedReservations() {
-	//"DatadeInicio" and "DataFinal"
+	//"DatadeInicio" and "DatadeFim"
 	datadeInicio1, _ := time.Parse(time.RFC3339, "1969-12-8T21:00:00-01:00")
 	datadeFim1, _ := time.Parse(time.RFC3339, "1969-12-10T21:00:00-05:00")
 	datadeInicio2, _ := time.Parse(time.RFC3339, "1969-12-20T21:00:00-02:00")
@@ -41,6 +43,7 @@ func SeedReservations() {
 	}
 }
 
+//Truncate drop database and reinsert the fixture
 func Truncate() {
 	collection := helper.ConnectDB()
 	ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
@@ -48,6 +51,15 @@ func Truncate() {
 	SeedReservations()
 }
 
-func TestMain(m *testing.M) {
+func TestMain(m *testing.M) int {
+	//tempDir is a new space to store new datas temporary to mongo
+	tempDir, err := ioutil.TempDir("", "testing")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	exitVal := m.Run()
+	defer os.RemoveAll(tempDir) // clean up
 	os.Exit(TestMain(m))
+	return exitVal
 }
